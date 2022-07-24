30bcc0ddb0ea998dbb2a259684a559dc4280acb4
antmar0417 <tonyboy@live.se>
2022-07-24 20:40:39 +0200

    changed the language

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calc() {
-  var amount = parseFloat(document.getElementById("amount").value);
-  var interest =
+  const amount = parseFloat(document.getElementById("amount").value);
+  const interest =
     parseFloat(document.getElementById("interest").value) / 100 + 1;
-  var year = document.getElementById("year").value;
-  var amountAfter = amount * Math.pow(interest, year);
-  var calc = (Math.pow(interest, year) - 1) / (interest - 1);
-  var res = amountAfter / calc / 12;
+  const year = document.getElementById("year").value;
+  const amountAfter = amount * Math.pow(interest, year);
+  const calc = (Math.pow(interest, year) - 1) / (interest - 1);
+  const res = amountAfter / calc / 12;
 
   document.getElementById("result").value = res.toFixed(2);
 }
