62b360b9d8edc09ffb523a9d8a88c6f6280e27e1
antmar0417 <tonyboy@live.se>
2022-07-24 21:23:38 +0200

    added a cash deposit field

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ function calc() {
   const interest =
     parseFloat(document.getElementById("interest").value) / 100 + 1;
   const year = document.getElementById("year").value;
-  const amountAfter = amount * Math.pow(interest, year);
+  const cashDeposit =
+    (parseFloat(document.getElementById("cashDeposit").value) / 100) * amount;
+  const amountAfter = (amount - cashDeposit) * Math.pow(interest, year);
   const calc = (Math.pow(interest, year) - 1) / (interest - 1);
   const res = amountAfter / calc / 12;
 
   document.getElementById("result").value = res.toFixed(2);
+  document.getElementById("cashDeposit").value = `${cashDeposit} USD`;
 }
