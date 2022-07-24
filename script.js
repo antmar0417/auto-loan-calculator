81815fa139ce2a9feef7380b2aaf277fb5f18604
antmar0417 <tonyboy@live.se>
2022-07-24 22:49:45 +0200

    syled the content

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function calc() {
-  const amount = parseFloat(document.getElementById("amount").value);
+  const amount = document.getElementById("amount").value;
+  checkAmount(amount);
   const interest =
     parseFloat(document.getElementById("interest").value) / 100 + 1;
   const year = document.getElementById("year").value;
-  const cashDeposit =
-    (parseFloat(document.getElementById("cashDeposit").value) / 100) * amount;
+  const cashDepositPercent = document.getElementById("cashDeposit").value;
+  checkInterest(cashDepositPercent);
+  const cashDeposit = (parseFloat(cashDepositPercent) / 100) * amount;
   const amountAfter = (amount - cashDeposit) * Math.pow(interest, year);
   const calc = (Math.pow(interest, year) - 1) / (interest - 1);
   const res = amountAfter / calc / 12;
 
   document.getElementById("result").value = res.toFixed(2);
-  document.getElementById("cashDeposit").value = `${cashDeposit} USD`;
+  document.getElementById("cashDeposit").value = cashDeposit;
+}
+
+function checkAmount(amount) {
+  if (amount == "") {
+    window.alert("You must provide an amount!");
+  }
+}
+
+function checkInterest(cashDepositPercent) {
+  if (cashDepositPercent < 20) {
+    window.alert("The Deposit should be atleast 20 Percent!");
+  }
 }
